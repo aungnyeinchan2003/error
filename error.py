916559e9d83b4e56b4211a8f2eb327413e05f608
aungnyeinchan2003 <66313632+aungnyeinchan2003@users.noreply.github.com>
2020-06-02 19:45:36 +0630

    Add files via upload

---
+++ error.py
@@ -0,0 +1,42 @@
+print("Error Hacking Tool.")
+print("===================")
+option=1
+while option != 4:
+    print("[1]. Facebook")
+    print("[2]. Instalgram")
+    print("[3]. Viber")
+    print("[4]. exit")
+    print("[5]. About")
+    option = int(input("Select an option: "))
+    if option == 1:
+        id = input("Enter your Target ID: ")
+        i = 1
+        while i <= 10:
+            print("typing password......")
+            i+=1
+        print("Your target is successfully hacked.")  
+        print("Password is ***********")
+    elif option == 2:
+        username = input("Enter your target username: ")
+        j = 1
+        while j <= 10:
+            print("typing password.......")
+            j+=1
+        print("Your target is successfully hacked.")  
+        print("Password is ***********")
+    elif option == 3:
+        phone = input("Enter target phone number: ")
+        c = 1
+        while c <= 10:
+            print("typing password.......")
+            c+=1
+        print("Your target is successfully hacked.")  
+        print("Password is ***********")
+    elif option == 5:
+        print("Tool Name : Error")
+        print("Release date : June 2, 2020")
+        print("Developer : Aung Nyein Chan")
+        print("Developed language : Python")
+        print("Update : not avliable")
+print("Thanks for using my program")
+
